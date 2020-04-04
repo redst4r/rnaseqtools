32f1dd582d6a20d0ff58c5490f516f69d9f4580c
redst4r <redst4r@web.de>
2020-04-04 10:09:59 -0700

    added fastq reading

---
+++ rnaseqtools/fastqio.py
@@ -0,0 +1,33 @@
+import gzip
+import itertools
+"""
+here's a fast iterator over the sequences, doesnt do the checking done by skbio
+"""
+
+
+def read_fastq_seqs_bare(filepath):
+    """returns the bare 4 lines of the fastq entry, no postprocesing (i.e. newline stripping)"""
+    with gzip.open(filepath, 'rt') as fh:
+        for seq_header, seq, qual_header, qual in itertools.zip_longest(*[fh] * 4):
+            if any([line is None for line in (seq_header, seq, qual_header, qual)]):
+                raise Exception(
+                    "Number of lines in FASTQ file must be multiple of four "
+                    "(i.e., each record must be exactly four lines long).")
+
+            if not seq_header.startswith('@'):
+                raise Exception("Invalid FASTQ sequence header: %r" % seq_header)
+            if qual_header != '+\n':
+                raise Exception("Invalid FASTQ quality header: %r" % qual_header)
+            if qual == '\n':
+                if not seq =='\n':  # sometimes theres just no sequence, hence also quality socre misses
+                    raise Exception("FASTQ record is missing quality scores.")
+            yield seq_header, seq, qual_header, qual
+
+
+def read_fastq_seqs(filepath):
+    """
+    reading the fastq entries, stripping newlines, returning only read_id and read_sequence
+    """
+    for seq_header, seq, qual_header, qual in read_fastq_seqs_bare(filepath):
+        read_id = seq_header[1:].split(" ")[0]
+        yield read_id, seq.rstrip('\n')
