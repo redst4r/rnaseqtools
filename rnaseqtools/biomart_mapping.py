5b762a486c04da5d0a98d42625537a7dcc67b6ae
redst4r <redst4r@web.de>
2021-01-15 02:00:36 -0800

    biomar: major rework of main function (now similar to biomart::getBM())

--- rnaseqtools/biomart_mapping.py
+++ rnaseqtools/biomart_mapping.py
@@ -112,51 +112,47 @@ def biomart_query_all(verbose=False, extra_fields=None, force_download=False):
 
     return df
 
-def biomart_query(id_list:list, id_type:str, batchsize=10000, verbose=False):
+
+def batch(some_iterable, batchsize):
     """
-    pulls down the symbol/entrez/transcriptlength for each ensebml-rtanscript_id given
+    splits the iterable into a couple of chunks of size n
+    handy for iterating over batches
+    :param some_iterable:  iterable to be chunked/batched
+    :param batchsize: batchSize
+    :return: gnerator over iterables
     """
+    assert isinstance(some_iterable, collections.Iterable)  # TODO this does not guard against np.arrays as they are also iterable (over single elements)
+    l = len(some_iterable)
+    for ndx in range(0, l, batchsize):
+        yield some_iterable[ndx:min(ndx + batchsize, l)]
 
-    assert id_type in ['ensembl_gene_id', 'ensembl_transcript_id']
 
-    s = biomart.BioMart(host='uswest.ensembl.org')
+def biomart_query_new(attributes:list, filter:str, filter_values:list, batchsize=10000, verbose=False):
+    """
+    query biomart similar to the R-function getBM()
 
-    "we have to batch the call if the list of queries is long"
-    def batch(some_iterable, batchsize):
-        """
-        splits the iterable into a couple of chunks of size n
-        handy for iterating over batches
-        :param some_iterable:  iterable to be chunked/batched
-        :param batchsize: batchSize
-        :return: gnerator over iterables
-        """
-        assert isinstance(some_iterable, collections.Iterable)  # TODO this does not guard against np.arrays as they are also iterable (over single elements)
-        l = len(some_iterable)
-        for ndx in range(0, l, batchsize):
-            yield some_iterable[ndx:min(ndx + batchsize, l)]
-
+    :param attributes: which attributes to retrieve from biomart
+    :param filter: field which to filter on
+    :param filter_values: return only entries where filter \in filter_values
+    """
+    s = biomart.BioMart(host=HOST)
 
     batch_results = []
-    n_batches = int(np.ceil(len(id_list)/ batchsize))
-    for id_batch in tqdm.tqdm(batch(id_list, batchsize=batchsize), total=n_batches):
+    n_batches = int(np.ceil(len(filter_values)/ batchsize))
+    for id_batch in tqdm.tqdm(batch(filter_values, batchsize=batchsize), total=n_batches):
 
         s.new_query()
         s.add_dataset_to_xml('hsapiens_gene_ensembl')
 
         # what we want to get back
-        # s.add_attribute_to_xml('entrezgene')
-        s.add_attribute_to_xml('hgnc_symbol')
-        s.add_attribute_to_xml('ensembl_gene_id')
-        s.add_attribute_to_xml('transcript_length')
-        s.add_attribute_to_xml('ensembl_transcript_id')
-        s.add_attribute_to_xml('transcript_biotype')
-        s.add_attribute_to_xml('entrezgene')
+        for a in attributes:
+            s.add_attribute_to_xml(a)
 
         # the query should be comma separated
         # better make sure theres no whitespace
         query = ",".join([_.strip() for _ in id_batch])
 
-        s.add_filter_to_xml(id_type, query)
+        s.add_filter_to_xml(filter, query)
         xml = s.get_xml()
 
         if verbose:
@@ -165,10 +161,32 @@ def biomart_query(id_list:list, id_type:str, batchsize=10000, verbose=False):
         res = s.query(xml)
 
         df = pd.read_csv(io.StringIO(res), sep='\t', header=None)
-        df.columns=['hgnc_symbol', 'ensembl_gene_id', 'transcript_length', 'ensembl_transcript_id', 'transcript_biotype', 'entrezgene']
+        df.columns = attributes
         df = df.drop_duplicates()
         batch_results.append(df)
 
     df = pd.concat(batch_results, axis=0).drop_duplicates()
 
     return df
+
+
+def biomart_query(id_list:list, id_type:str, batchsize=10000, verbose=False):
+    """
+    THIS IS SOME LEGACY FUNTION AS OF 2021/01/14. JUST CALLS biomart_query_new
+    which has the proper naming of function arguments (as R's getBM())
+
+    pulls down the symbol/entrez/transcriptlength for each ensebml-rtanscript_id given
+    """
+
+    warnings.warn('This function is deprecated. Use biomart_query_new', DeprecationWarning)
+    assert id_type in ['ensembl_gene_id', 'ensembl_transcript_id']
+
+    attributes = [
+        'hgnc_symbol',
+        'ensembl_gene_id',
+        'transcript_length',
+        'ensembl_transcript_id',
+        'transcript_biotype',
+        'entrezgene',
+    ]
+    return biomart_query_new(attributes, filter=id_type, filter_values=id_list, batchsize=batchsize)
