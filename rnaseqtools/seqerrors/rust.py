7c1763e35b91f3e9e76075c904bd7237421f08e6
redst4r <redst4r@web.de>
2022-06-12 04:34:31 -0700

    even more wrapper code

--- rnaseqtools/seqerrors/rust.py
+++ rnaseqtools/seqerrors/rust.py
@@ -14,7 +14,6 @@ def mk_rustcall_cbumi(fastq_glob, whitelist_file, outfile, topn):
     # print(s1)
     os.system(s1)
 
-
 def mk_rustcall_cbumi_full(fastq_glob, whitelist_file, outfile, topn):
     s1 = f"{PREFIX} cargo run --release -- --output {outfile} cb-umi-exact -w {whitelist_file} --ntop {topn}   {fastq_glob}"
     # print(s1)
@@ -27,14 +26,12 @@ def mk_rustcall_cbumi_cell(busfile, outfile, nmax, aggr: bool):
     # print(s1)
     os.system(s1)
 
-def mk_rustcall_cbumi_cell_gene(busfolder, outfile, nmax, aggr: bool):
-    s1 = f"{PREFIX} cargo run --release -- --output {outfile} cb-umi-cell-gene --nmax {nmax} --busfolder {busfolder}"
+def mk_rustcall_cbumi_cell_gene(busfolder, outfile, nmax, aggr: bool, t2g):
+    s1 = f"{PREFIX} cargo run --release -- --output {outfile} cb-umi-cell-gene --nmax {nmax} --busfolder {busfolder} --t2g {t2g}"
     if aggr:
         s1 = f'{s1} --aggr'
     # print(s1)
     os.system(s1)
-
-
 
 
 def rust_output_to_error_estimate(df_rust):
@@ -51,18 +48,6 @@ def rust_output_to_error_estimate(df_rust):
 
 def rust_output_to_error_estimate_binomial(df_rust):
     return beta_binomial_error_estimates(df_rust)
-# def rust_output_to_error_estimate_binomial(df_rust):
-#     n_bases = len([_ for _ in df_rust.columns if _.startswith('position_')])
-#     df_error2 = []
-#     for i in range(n_bases):
-#         col = f'position_{i}'
-#         _df = df_rust[['n_real', col]].rename({col:'n_shadow'}, axis=1)
-#         s = beta_binomial_error_estimates(_df)
-#         s['position'] = i
-#         df_error2.append(s)
-#     df_error2 = pd.DataFrame(df_error2)
-#     return df_error2
-
 
 def rust_read_cb_results(samplename, DIRECTORY):
     # reading the precompuated error estimates in the directory
@@ -104,6 +89,13 @@ def _load_rust_cb_cell_aggregate(fname, remove_singletons):
         df_raw['n_total'] = df_raw['n_total'] - df_raw['singeltons']
 
     df_error = beta_binomial_error_estimates(df_raw)
+    return df_error
+
+def rust_read_tso_results(samplename, DIRECTORY):
+    # reading the precompuated error estimates in the directory
+    results_df = pd.read_csv(f'{DIRECTORY}/{samplename}/tso.csv')
+    df_error  = beta_binomial_error_estimates(results_df)
+    df_error['samplename'] = samplename
     return df_error
 
 def load_rust_cb_cell_aggregate(samplename, remove_singletons, DIRECTORY):
